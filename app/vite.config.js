1df871aac498fbae95fd35104b3f2821039f00cf
Harshitjoshi133 <116452069+Harshitjoshi133@users.noreply.github.com>
2024-06-13 11:03:43 +0530

    Deployment Code

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Foody-zone/app/', // Base path for GitHub Pages
+  base: '/Foody-zone/', // Base path for GitHub Pages
   plugins: [react()]
 });
