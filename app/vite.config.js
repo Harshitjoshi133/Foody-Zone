5a04a5b3cc619392a1c73fcc4c8750c7131b28b5
Harshitjoshi133 <116452069+Harshitjoshi133@users.noreply.github.com>
2024-06-18 09:16:49 +0530

    For VerCel Deployment

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,8 +1,8 @@
 // app/vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Foody-zone/', // Base path for GitHub Pages
-  plugins: [react()]
-});
+  plugins: [react()],
+})
